56dd9d9610b0c11cbd0ba74256f77f91226e2324
mhiss11 <97799866+mhiss11@users.noreply.github.com>
2022-04-10 14:34:56 -0400

    Update scheduler script.js

--- scheduler script.js
+++ scheduler script.js
@@ -18,8 +18,8 @@ const months = ["January", "February", "March", "April", "May", "June",
 
 
 const timeEntryName = "WorkDaySchedule";
-const firstHour = 8;
-const lastHour = 17;
+const firstHour = 10;
+const lastHour = 23;
 const hourMap = ["12AM", "1AM", "2AM", "3AM", "4AM", "5AM", "6AM", "7AM", "8AM", "9AM", "10AM", "11AM", "12PM",
     "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM", "10PM", "11PM"];
 
@@ -178,3 +178,4 @@ function saveClick() {
 
     localStorage.setItem(timeEntryName, JSON.stringify(timeEntry));
 }
+
